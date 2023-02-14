1d9baf9d6e31063631e3dee25c0df49289aa9c27
caseyjramirez <caseyjramirez5@gmail.com>
2023-02-14 10:22:02 -0600

    added sequence to Student.java

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,4 +1,30 @@
 package com.example.demo;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity(name = "Student")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Integer age;
 }
