4b5f403c3d6c7fd6cfa9a89823ae181a7042ba9d
caseyjramirez <caseyjramirez5@gmail.com>
2023-02-15 12:36:47 -0600

    Updated the StudentRepository.java with custom Queries and Object Modification.

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class Application {
@@ -17,13 +19,42 @@ public class Application {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-//            System.out.println("********* ARGS *********" + args);
 
             Student casey = new Student("Casey", "Ramirez", "testing@email.com", 23);
-            studentRepository.save(casey);
+            Student casey1 = new Student("Casey", "Xavier", "testing1@email.com", 23);
+            Student casey2 = new Student("Casey", "Badger", "testing2@email.com", 23);
+            Student casey3 = new Student("Casey", "Clairo", "testing3@email.com", 23);
+            Student jonny = new Student("Jonny", "Randall", "jonny@email.com", 30);
+            studentRepository.saveAll(List.of(casey, casey1, casey2, casey3, jonny));
+            System.out.println("**************************");
 
-            List<Student> allStudents = studentRepository.findAll();
-            System.out.println(allStudents);
+            studentRepository
+                    .findStudentByEmail("jonny@email.com")
+                    .ifPresentOrElse(
+                            System.out::println,
+                            () -> {System.out.println("NOT FOUND");
+                    });
+
+            studentRepository
+                    .findStudentsByFirstName("Casey")
+                    .ifPresentOrElse(
+                            students -> {
+                                students.forEach(System.out::println);
+                            },
+                            () -> {
+                                System.out.println("Not Present");
+                            }
+                    );
+
+            List<Student> query = studentRepository.findStudentsByAgeEqualsAndFirstNameEquals(30, "Jonny");
+            List<Student> query1 = studentRepository.findStudentsByAgeEqualsAndFirstNameEquals(25, "Casey");
+            System.out.println(query);
+            System.out.println(query1);
+
+            studentRepository.nativeQuery("Casey", 18)
+                    .ifPresent(students -> students.forEach(System.out::println));
+
+            System.out.println(studentRepository.deleteStudentByEmail("testing@email.com"));
         };
     }
 
--- src/main/java/com/example/demo/StudentRepository.java
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -1,6 +1,28 @@
 package com.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
+    @Query("SELECT s from Student s WHERE s.age = ?1 AND s.firstName = ?2")
+    List<Student> findStudentsByAgeEqualsAndFirstNameEquals(Integer age, String fistName);
+    @Query("SELECT s FROM Student s WHERE s.firstName = ?1")
+    Optional<List<Student>> findStudentsByFirstName(String firstName);
+
+// SQL NATIVE QUERY
+    @Query(value = "SELECT * FROM Student WHERE first_name = :firstName AND age >= :age", nativeQuery = true)
+    Optional<List<Student>> nativeQuery(@Param("firstName") String firstName, @Param("age") Integer age);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE Student s WHERE s.email = :email")
+    int deleteStudentByEmail(@Param("email") String email);
 }
