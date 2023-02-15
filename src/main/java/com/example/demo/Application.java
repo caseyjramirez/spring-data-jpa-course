92fcc63dcecea01fd6b7463f70f4d6336ba7e8f4
caseyjramirez <caseyjramirez5@gmail.com>
2023-02-15 10:08:12 -0600

    Initialized the StudentRepository.java

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+//            System.out.println("********* ARGS *********" + args);
+
+            Student casey = new Student("Casey", "Ramirez", "testing@email.com", 23);
+            studentRepository.save(casey);
+
+            List<Student> allStudents = studentRepository.findAll();
+            System.out.println(allStudents);
+        };
     }
 
 }
--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -6,7 +6,10 @@ import javax.persistence.*;
 
 @Entity(name = "Student")
 @Table(name = "student", uniqueConstraints = {
-        @UniqueConstraint(name = "student_email_unique", columnNames = "email")
+        @UniqueConstraint(
+                name = "student_email_unique",
+                columnNames = "email"
+        )
 })
 @Getter
 @Setter
@@ -39,4 +42,11 @@ public class Student {
 
     @Column(name="age", nullable = false)
     private Integer age;
+
+    public Student(String firstName, String lastName, String email, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
 }
